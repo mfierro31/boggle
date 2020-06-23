e3bbdac1e66c48788bdbb5bbef90493eac76ed0d
Mike Fierro <mfier@MacBook-Pro.local>
2020-06-23 12:58:29 -0700

    added tests

--- test.py
+++ test.py
@@ -7,26 +7,55 @@ app.config['TESTING'] = True
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class FlaskTests(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        boggle = Boggle()
-        boggle_board = boggle.make_board()
-
     def test_home_page(self):
         with app.test_client() as client:
             res = client.get('/')
+
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 200)
 
-            self.assertIn('<div class="display-1 mb-4">Boggle!</div>', html)
+            self.assertIn('border border-dark">', html)
 
-    def test_submit_guess(self):
+            self.assertIn('board', session)
+            self.assertIn('times_visited', session)
+            self.assertIn('scores', session)
+            self.assertIn('guess', session)
+
+
+    def test_valid_word(self):
         with app.test_client() as client:
-            res = client.get('/check-word', data={'guess': 'if'})
-            html = res.get_data(as_text=True)
+            with client.session_transaction() as sess:
+                sess['board'] = [["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"]]
 
-            self.assertEqual(res.status_code, 200)
+                sess['guess'] = ['dog']
 
-            self.assertIn('<div class="display-1 mb-4">Boggle!</div>', html)
-            self.assertIn('<div class="alert', html)
+        response = client.get('/check-word?guess=cat')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_not_on_board(self):
+        with app.test_client() as client:
+            client.get('/')
+            res = client.get('/check-word?guess=impossible')
+            self.assertEqual(res.json['result'], 'not-on-board')
+
+    def test_not_a_word(self):
+        with app.test_client() as client:
+            client.get('/')
+            res = client.get('/check-word?guess=crflm')
+            self.assertEqual(res.json['result'], 'not-a-word')
+
+    def test_already_used(self):
+        with app.test_client() as client:
+            client.get('/')
+
+            with client.session_transaction() as sess:
+                sess['guess'] = ['dog']
+
+        res = client.get('/check-word?guess=dog')
+        self.assertEqual(res.json['result'], 'word-already-used')
+
